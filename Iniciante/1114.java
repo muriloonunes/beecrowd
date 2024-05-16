6772d5af472d0709a09e28682e2ebc99a7c10b82
Murilo <57834900+muriloonunes@users.noreply.github.com>
2024-05-16 19:00:21 -0300

    happy coding

--- Iniciante/1114.java
+++ Iniciante/1114.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int a = ler.nextInt();
-        int b = ler.nextInt();
-        int PROD = (a*b);
-        System.out.println("PROD = "+PROD);
+        int senha = 0;
+        while (senha != 2002) {
+            senha = ler.nextInt();
+            if (senha != 2002) {
+                System.out.println("Senha Invalida");
+            }
+        }
+        System.out.println("Acesso Permitido");
     }
 } 
