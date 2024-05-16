e6ded06b37d5de8a2113c37d0f54c33b1c91d708
Murilo <57834900+muriloonunes@users.noreply.github.com>
2024-05-16 19:02:12 -0300

    happy coding

--- Iniciante/1003.java
+++ Iniciante/1003.java
@@ -3,18 +3,11 @@ package Iniciante;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int c = ler.nextInt();
-        int contador = 0;
-        String palavra;
-        double tempo;
-        while (contador < c) {
-            palavra = ler.next();
-            tempo = (double) palavra.length() /100;
-            System.out.printf("%.2f\n",tempo);
-            contador++;
-        }
+        int a = ler.nextInt();
+        int b = ler.nextInt();
+        int SOMA = (int) (a+b);
+        System.out.println("SOMA = "+SOMA);
     }
 }
