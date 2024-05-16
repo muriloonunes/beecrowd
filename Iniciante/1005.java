40d0ac2fd148e4393d77b1077103092d66104c98
Murilo <57834900+muriloonunes@users.noreply.github.com>
2024-05-16 19:03:24 -0300

    happy coding

--- Iniciante/1005.java
+++ Iniciante/1005.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int a = ler.nextInt();
-        int b = ler.nextInt();
-        int PROD = (a*b);
-        System.out.println("PROD = "+PROD);
+        double a,b,MEDIA;
+        a = ler.nextDouble();
+        b = ler.nextDouble();
+        MEDIA = (a*3.5 + b*7.5) / 11;
+        System.out.printf("MEDIA = %.5f\n",MEDIA);
     }
 } 
