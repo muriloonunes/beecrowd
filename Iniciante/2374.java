e01fc761a2752808ad332322979f9c7998f24ff5
Murilo <57834900+muriloonunes@users.noreply.github.com>
2024-05-16 18:49:46 -0300

    happy coding

---
+++ Iniciante/2374.java
@@ -0,0 +1,14 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int n = ler.nextInt();
+        int m = ler.nextInt();
+        int diferenca = n-m;
+        System.out.println(diferenca);
+    }
+}
