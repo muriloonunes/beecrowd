0923482a35046e37422d85ced4ab32d0882e933c
Murilo <57834900+muriloonunes@users.noreply.github.com>
2024-05-16 18:41:33 -0300

    happy coding

---
+++ Iniciante/2936.py
@@ -0,0 +1,13 @@
+curupira = int(input())
+totalCurupira = curupira*300
+boitata = int(input())
+totalBoitata = boitata*1500
+boto = int(input())
+totalBoto = boto*600
+mapinguari = int(input())
+totalMapinguari = mapinguari*1000
+iara = int(input())
+totalIara = iara*150
+total = totalCurupira+totalBoitata+totalBoto+totalMapinguari+totalIara+225
+
+print(total)
