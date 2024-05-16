37dc1455b505aec8034a3c9cc18fd2bb779fd229
Murilo <57834900+muriloonunes@users.noreply.github.com>
2024-05-16 19:06:02 -0300

    happy coding

--- Iniciante/1073.java
+++ Iniciante/1073.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int a = ler.nextInt();
-        int b = ler.nextInt();
-        int PROD = (a*b);
-        System.out.println("PROD = "+PROD);
+        int numero = 0;
+        while (numero < 5 || numero > 2000) {
+            numero = ler.nextInt();
+        }
+        int n1 = 2;
+        double resultado;
+
+        while (n1 <= numero) {
+            resultado = Math.pow(n1, 2);
+            System.out.printf("%d^2 = %.0f\n",n1,resultado);
+            n1 += 2;
+        }
     }
-} 
+}
