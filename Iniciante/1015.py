b4789c5843d9133b79c822ea1f8ace2b84bb7467
Murilo <57834900+muriloonunes@users.noreply.github.com>
2024-05-16 18:35:24 -0300

    happy coding

---
+++ Iniciante/1015.py
@@ -0,0 +1,12 @@
+import math
+
+linha1 = input().split()
+x1 = float(linha1[0])
+y1 = float(linha1[1])
+
+linha2 = input().split()
+x2 = float(linha2[0])
+y2 = float(linha2[1])
+
+distancia = math.sqrt((math.pow((x2-x1),2))+math.pow((y2-y1),2))
+print("%.4f"%distancia)
