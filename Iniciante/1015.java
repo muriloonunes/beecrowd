c0d3e2044863c1c3c688c5fa30b484fa06f29c34
Murilo <57834900+muriloonunes@users.noreply.github.com>
2024-05-16 18:32:41 -0300

    happy coding

---
+++ Iniciante/1015.java
@@ -0,0 +1,16 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        double x1 = ler.nextDouble();
+        double y1 = ler.nextDouble();
+        double x2 = ler.nextDouble();
+        double y2 = ler.nextDouble();
+        double distancia = Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2)));
+        System.out.printf("%.4f\n",distancia);
+    }
+}
