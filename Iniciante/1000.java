619eac0438576de2ff92423327e75a722c057e73
Murilo <57834900+muriloonunes@users.noreply.github.com>
2024-05-16 19:01:01 -0300

    happy coding

--- Iniciante/1000.java
+++ Iniciante/1000.java
@@ -1,20 +1,7 @@
 package Iniciante;
 
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
-        int c = ler.nextInt();
-        int contador = 0;
-        String palavra;
-        double tempo;
-        while (contador < c) {
-            palavra = ler.next();
-            tempo = (double) palavra.length() /100;
-            System.out.printf("%.2f\n",tempo);
-            contador++;
-        }
+        System.out.println("Hello World!");
     }
 }
